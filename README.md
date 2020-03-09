af62ed4ae95d0fb933233c2217f65ce4bf96eb7b
Bashanta Dahal <basantad2@gmail.com>
2020-03-08 22:28:11 -0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# test-react-app
-Created with CodeSandbox
---
+++ src/Components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = ({ label }) => <div className="header">{label}</div>;
+
+export default Header;
